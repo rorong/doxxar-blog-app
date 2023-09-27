8e403c54ba3a87502a934e0fbe068028c3cc6f58
rorong <rordev@ongraph.com>
2023-09-27 15:15:59 +0530

    Added new User , Blog and Devise View

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  root to: "rails_admin/main#dashboard"
+  devise_for :users
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
